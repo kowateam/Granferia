0263e0ac295de5296f36aa7cc9f8b697577427e0
unknown <romegomez29@gmail.com>
2022-05-02 00:23:48 -0300

    otros cambios

--- src/main/resources/static/js/empLanding.js
+++ src/main/resources/static/js/empLanding.js
@@ -1,20 +1,6 @@
 
 
-let clickHidden=document.querySelector(".clickHidden")
-let clickHidden2=document.querySelector(".clickHidden2")
-let idClickHidden= document.querySelector("#idClickHidden")
-let idClickHidden2= document.querySelector("#idClickHidden2")
-let footer= document.querySelector("footer")
-clickHidden.addEventListener("click", function(){
-    idClickHidden.classList.toggle("hidden")
-    idClickHidden2.classList.toggle("hidden")
-    footer.style.display="none"
-})
-clickHidden2.addEventListener("click",function(){
-    idClickHidden.classList.toggle("hidden")
-    idClickHidden2.classList.toggle("hidden")
-    footer.style.display="block"
-})
+
 let idUsuario=document.querySelectorAll(".idUsuario")
 let idEmprendimiento=document.querySelectorAll(".idEmprendimiento")
 let guargarvariable=document.querySelectorAll(".btn-seguir-emprendimiento");
